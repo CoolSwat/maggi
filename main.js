6a884cb52da32556c17063343d0c235e4b37524a
unknown <smate@curologic.com>
2015-08-22 16:52:26 +0530

    running pepper again

--- main.js
+++ main.js
@@ -3,12 +3,37 @@ var esUtil=require('./esUtil')
 var mongoutil=require("./mongoUtil");
 var fs=require("fs");
 var async=require("async");
-var cawlConfig=require('./scripts/caratlane.json')
-
+var cawlConfig=require('./scripts/pepperfry.json')
+//var mongoToMysql=require('./mongoToMysql');
 //initialize mongo util for only 1 model
 var objmongoutil = new mongoutil('localhost','sos',cawlConfig.sitemap.model);
 
-start(cawlConfig.sitemap);
+//After inserting crawled data in to mongo db call this function.
+//	mongoToMysql.startConvert(cawlConfig);
+
+var preProcessor=function(callback){
+    objmongoutil.getLastCrawlDate().then(function(date){
+       var lastDate= new Date(date)
+       var oldCollectionName=objmongoutil.objDataModel.collection.name;
+        var newCollectionName=oldCollectionName+"_"+lastDate.getDate()+"_"+lastDate.getMonth()
+    objmongoutil.renameCollection(oldCollectionName,newCollectionName)    
+        console.log(newCollectionName)
+        callback()
+    },function(error){
+        console.log(error)
+        callback()
+    })   
+}
+
+async.series(preProcessor,start(cawlConfig.sitemap),function(err){
+    if(!err)
+        console.log('completed successfuly')
+    else
+        console.log('something went wrong')
+})
+
+
+//start(cawlConfig.sitemap);
 var i=0;
 function start(sitemap){	
 	utili.getLinks(sitemap.siteMapUrl,sitemap.level)	
@@ -36,3 +61,4 @@ function start(sitemap){
 		} 
 	});
 }
+
